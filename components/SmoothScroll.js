95ac28703b0acb1aeba119fbf7e4d6fa18580006
Samveg Mehta <samveg.mehta@ansibytecode.com>
2026-03-14 14:57:57 +0530

    fix: update lenis package name

--- components/SmoothScroll.js
+++ components/SmoothScroll.js
@@ -1,6 +1,6 @@
 'use client'
 import { useEffect } from 'react'
-import Lenis from '@studio-freight/lenis'
+import Lenis from 'lenis'
 import { gsap } from 'gsap'
 import { ScrollTrigger } from 'gsap/ScrollTrigger'
 
